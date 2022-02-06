8d517ef520e41f613cb913d6eb176ad7e31ddd04
Developer Arif <70232458+developerarif01@users.noreply.github.com>
2022-02-07 00:25:00 +0600

    category&subčategory created

--- resources/views/BackEnd/category/subcategory_view.blade.php
+++ resources/views/BackEnd/category/subcategory_view.blade.php
@@ -8,7 +8,7 @@
             <div class="col-8">
                 <div class="box">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Brand List</h3>
+                        <h3 class="box-title">Sub Category List</h3>
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
@@ -16,26 +16,24 @@
                             <table id="example1" class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
-                                        <th>Brand En</th>
-                                        <th>Brand BD</th>
-                                        <th>Image</th>
+                                        <th> Category </th>
+                                        <th>Sub Category En</th>
+                                        <th>Sub Category BD</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-
-                                    @foreach ($brands as $item )
+                                    @foreach ($subcategory as $item )
                                     <tr>
-                                        <td>{{$item->brand_name_en}} </td>
-                                        <td>{{$item->brand_name_bd}}</td>
-                                        <td><img src="{{asset($item->brand_image)}}" alt="No Image" style="width:70px;height:70px;"></td>
+                                        <td>{{$item['category']['category_name_en']}} </td>
+                                        <td>{{$item->subcategory_name_en}} </td>
+                                        <td>{{$item->subcategory_name_bd}}</td>
                                         <td style="display:flex; justify-content:space-evenly;">
-                                            <a href="{{route('brand.edit',$item->id)}} " class="btn btn-info" title="Edit Data" > <i class="fa fa-pencil"></i></a>
-                                            <a href="{{route('brand.delete',$item->id)}}" class="btn btn-danger" id="delete" title="Delete Data"><i class="fa fa-trash"></i></a>
+                                            <a href="{{route('subcategory.edit',$item->id)}} " class="btn btn-info" title="Edit Data" > <i class="fa fa-pencil"></i></a>
+                                            <a href="{{route('subcategory.delete',$item->id)}}" class="btn btn-danger" id="delete" title="Delete Data"><i class="fa fa-trash"></i></a>
                                         </td>
                                     </tr>
                                     @endforeach
-
                                 </tbody>
                             </table>
                         </div>
@@ -46,55 +44,60 @@
             </div>
             <!-- /.col -->
 
-            {{--------------- Add Brand section -------------------------------}}
+            {{--------------- Add category section -------------------------------}}
 
             <div class="col-4">
                 <div class="box">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Add Brand</h3>
+                        <h3 class="box-title">Add SubCategory</h3>
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
                         <div class="table-responsive">
 
-                            <form method="post" action="{{route('brand.store')}} " enctype="multipart/form-data">
+                            <form method="post" action="{{route('subcategory.store')}}">
                                 @csrf
 
                                 <div class="form-group">
-                                    <h5>Brand Name English <span class="text-danger">*</span></h5>
+                                    <h5>Select Category<span class="text-danger">*</span></h5>
                                     <div class="controls">
-                                        <input type="text" name="brand_name_en" class="form-control">
-                                        @error('brand_name_en')
+                                        <select name="category_id"  class="form-control" aria-invalid="false">
+                                            <option value="" selected="" disabled >Select Category</option>
+                                            @foreach ($categories as $category )
+                                            <option value="{{ $category->id }}">{{ $category->category_name_en}} </option>
+                                            @endforeach
+                                        </select>
+                                        @error('category_id')
                                         <span class="text-danger" >{{ $message }}</span>
                                         @enderror
                                     </div>
                                 </div>
 
                                 <div class="form-group">
-                                    <h5>Brand Name Bangla <span class="text-danger">*</span></h5>
+                                    <h5>Sub Category Name English <span class="text-danger">*</span></h5>
                                     <div class="controls">
-                                        <input type="text" name="brand_name_bd" class="form-control">
-                                        @error('brand_name_bd')
+                                        <input type="text" name="subcategory_name_en" class="form-control">
+                                        @error('subcategory_name_en')
                                         <span class="text-danger" >{{ $message }}</span>
                                         @enderror
                                     </div>
                                 </div>
 
                                 <div class="form-group">
-                                    <h5>Brand Image <span class="text-danger">*</span></h5>
+                                    <h5>Sub Category Name Bangla <span class="text-danger">*</span></h5>
                                     <div class="controls">
-                                        <input type="file" name="brand_image" class="form-control">
-                                        @error('brand_image')
+                                        <input type="text" name="subcategory_name_bd" class="form-control">
+                                        @error('subcategory_name_bd')
                                         <span class="text-danger" >{{ $message }}</span>
                                         @enderror
                                     </div>
                                 </div>
-
 
                                 <div class="text-xs-right">
                                     <input type="submit" class="btn btn-rounded btn-primary" value="Add New">
                                 </div>
                             </form>
+
                         </div>
                     </div>
                 </div>
--- resources/views/admin/Layout/admin_master.blade.php
+++ resources/views/admin/Layout/admin_master.blade.php
@@ -14,6 +14,7 @@
     <!-- Style-->
     <link rel="stylesheet" href="{{asset('backend/css/style.css')}}">
     <link rel="stylesheet" href="{{asset('backend/css/skin_color.css')}}">
+    <link href="https://kit-pro.fontawesome.com/releases/v5.15.4/css/pro.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
 
 </head>
